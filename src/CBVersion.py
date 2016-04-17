74fe921a90aed1768d4c467bd7a633024b063525
Brett Olivier <brettolivier@gmail.com>
2016-04-17 23:57:46 +0200

    synch

--- src/CBVersion.py
+++ src/CBVersion.py
@@ -1,3 +1,4 @@
+<<<<<<< .mine
 """
 CBMPy: CBVersion module
 =======================
@@ -45,3 +46,100 @@ def current_version_tuple():
 __version__ = current_version()
 __DEBUG__ = True
 __DEBUG__ = False
+||||||| .r428
+"""
+CBMPy: CBVersion module
+=======================
+PySCeS Constraint Based Modelling (http://cbmpy.sourceforge.net)
+Copyright (C) 2009-2015 Brett G. Olivier, VU University Amsterdam, Amsterdam, The Netherlands
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+Author: Brett G. Olivier
+Contact email: bgoli@users.sourceforge.net
+Last edit: $Author: bgoli $ ($Id: CBVersion.py 432 2016-04-15 13:38:26Z bgoli $)
+
+"""
+# bump
+# preparing for Python 3 port
+from __future__ import division, print_function
+from __future__ import absolute_import
+#from __future__ import unicode_literals
+
+MAJOR = 0
+MINOR = 7
+MICRO = 4
+try:
+    STATUS = '$Rev: 432 $'.replace('Rev: ','').replace('$','').strip()
+except:
+    STATUS = ''
+
+def current_version():
+    return '%s.%s.%s.r%s' % (MAJOR, MINOR, MICRO, STATUS)
+
+def current_version_tuple():
+    return (MAJOR, MINOR, MICRO)
+
+__version__ = current_version()
+__DEBUG__ = True
+__DEBUG__ = False
+=======
+"""
+CBMPy: CBVersion module
+=======================
+PySCeS Constraint Based Modelling (http://cbmpy.sourceforge.net)
+Copyright (C) 2009-2015 Brett G. Olivier, VU University Amsterdam, Amsterdam, The Netherlands
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+Author: Brett G. Olivier
+Contact email: bgoli@users.sourceforge.net
+Last edit: $Author: bgoli $ ($Id: CBVersion.py 432 2016-04-15 13:38:26Z bgoli $)
+
+"""
+# bump 1
+# preparing for Python 3 port
+from __future__ import division, print_function
+from __future__ import absolute_import
+#from __future__ import unicode_literals
+ 
+MAJOR = 0
+MINOR = 7
+MICRO = 4
+try:
+    STATUS = '$Rev: 432 $'.replace('Rev: ','').replace('$','').strip()
+except:
+    STATUS = ''
+
+def current_version():
+    return '%s.%s.%s.r%s' % (MAJOR, MINOR, MICRO, STATUS)
+
+def current_version_tuple():
+    return (MAJOR, MINOR, MICRO)
+
+__version__ = current_version()
+__DEBUG__ = True
+__DEBUG__ = False
+>>>>>>> .r442
