3b9b30afcc91e15fd4f7365ec0de39f3de88341e
Brett Olivier <b.g.olivier@vu.nl>
2021-10-03 23:32:09 +0200

    updated description

--- setup.py
+++ setup.py
@@ -61,33 +61,19 @@ with open(os.path.join(local_path, 'cbmpy', 'CBConfig.py')) as F:
 
 mypackages = ['cbmpy', 'cbmpy.fluxmodules']
 
-readme = """ CBMPy
- =====
+readme = """
+PySCeS CBMPy (http://cbmpy.sourceforge.net) is a new platform for constraint
+based modelling and analysis. It has been designed using principles developed
+in the PySCeS simulation software project: usability, flexibility and accessibility. 
+Its architecture is both extensible and flexible using data structures that are intuitive to 
+the biologist (metabolites, reactions, compartments) while transparently translating these into 
+the underlying mathematical structures used in advanced analysis (LP's, MILP's).
 
- PySCeS CBMPy (http://cbmpy.sourceforge.net) is a new platform for constraint
- based modelling and analysis. It has been designed using principles developed
- in the PySCeS simulation software project: usability, flexibility and accessibility. Its architecture is both extensible and flexible using data structures that are intuitive to  the biologist (metabolites, reactions, compartments) while transparently translating these into the underlying mathematical structures used in advanced analysis (LP's, MILP's).
-
- PySCeS CBMPy implements popular analyses such as FBA, FVA, element/charge
- balancing, network analysis and model editing as well as advanced methods
- developed specifically for the ecosystem modelling: minimal distance methods,
- flux minimization and input selection. To cater for a diverse range of modelling
- needs PySCeS CBMPy supports user interaction via:
-
- - interactive console, scripting for advanced use or as a library for software development
- - GUI, for quick access to a visual representation of the model, analysis methods and annotation tools
- - SOAP based web services: using the Mariner framework much high level functionality is exposed for integration into web tools
-
- For more information on the development and use of PySCeS CBMPy feel free to contact me:
-
- PySCeS-CBMPy has been tested on Windows 7 and 8.1, Mac OSX and Ubuntu Linux 12.04, 14.04, 16.04. It is compatible with Python 3.6+ 
- 
-  To use follow the installation instructions given below and try the following in a Python shell::
-
- import cbmpy
- cmod = cbmpy.readSBML3FBC('cbmpy_test_core')
- cbmpy.doFBA(cmod)
-
+PySCeS CBMPy implements popular analyses such as FBA, FVA, element/charge
+balancing, network analysis and model editing as well as advanced methods
+developed specifically for the ecosystem modelling: minimal distance methods,
+flux minimization and input selection. To cater for a diverse range of modelling
+needs PySCeS CBMPy supports user interaction via:
 """
 
 description=""" CBMPy 
