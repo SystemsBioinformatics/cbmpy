b63ee5a555e3c450c42ec07112a4ab461ddeaa9c
bgoli <brettolivier@gmail.com>
2023-09-14 09:42:32 +0200

    tweaked example files

--- develop-fbcv3/QP/test_quadratic_obj.py
+++ develop-fbcv3/QP/test_quadratic_obj.py
@@ -2,45 +2,36 @@ import cbmpy
 
 # read my first SBML3 FBCv3 file
 
-print('\nREADING ...\n')
+print('\nOpen the_xenomorph.fbc3.xml to see how objectives and constraints are encoded.\n')
 # cmod3 = cbmpy.CBRead.readSBML3FBC('test_user_def_rc.xml')
 # cmod3 = cbmpy.CBRead.readSBML3FBC('e_coli_core.xml')
 # cmod3 = cbmpy.CBRead.readSBML3FBC('cbmpy_test_ecoli')
 
 cmod3 = cbmpy.CBRead.readSBML3FBC('the_xenomorph.fbc3.xml')
 
-# cmod3.getActiveObjective().setOperation('minimize')
+# Some methods of interest, there are more than these so explore them a bit
+
+# constraints
+cmod3.createUserDefinedConstraint
+cmod3.user_defined_constraints
+cbmpy.CBModel.UserDefinedConstraint.createConstraintComponent
+cbmpy.CBModel.UserDefinedConstraint.getConstraintComponent
+cbmpy.CBModel.UserDefinedConstraint.getConstraintComponentForVariable
+# convert old style constraints to new
+cmod3.convertUserConstraintsToUserDefinedConstraints
+
+# objectives
+cmod3.getActiveObjective().getLinearFluxObjectives
+cmod3.getActiveObjective().getQuadraticFluxObjectives
+cmod3.getActiveObjective().getQuadraticBivariateFluxObjectives
+
 
 cbmpy.doFBA(cmod3)
 
-
-# # this is all to test bounds, was used to fix Stevens bugs
-# rid =  cmod3.reactions[20].getId()
-# print('getReactionBounds')
-# print('Known ({}):'.format(rid), cmod3.getReactionBounds(rid))
-# print('UnKnown:', cmod3.getReactionBounds('Fred'))
-
-# print('getUpperBound')
-# print('Known ({}):'.format(rid), cmod3.getReaction(rid).getLowerBound())
-# print('Known ({}):'.format(rid), cmod3.getReaction(rid).getUpperBound())
-# try:
-    # print('UnKnown (this should fail):', cmod3.getReaction('fred').getUpperBound())
-# except AttributeError:
-    # print('UnKnown (this should fail)')
+print('Questions\nCan you encode a QP model that actually works, do I need to change any of the CPLEX options for a QP?')
 
 
-# R = cbmpy.CBModel.Reaction('newR')
-# cmod3.addReaction(R, create_default_bounds=True, silent=True)
-
-# for r in cmod3.reactions:
-    # lb1 = r.getLowerBound()
-    # ub1 = r.getUpperBound()
-    # b = cmod3.getReactionBounds(r.getId())
-    # lb2 = cmod3.getReactionLowerBound(r.getId())
-    # ub2 = cmod3.getReactionUpperBound(r.getId())
-    # print(r.getId(), b)
-    # print('lower pass:', lb1 == lb2)
-    # print('upper pass:', ub1 == ub2)
+# # this is all to test bounds, was used to debug stuff
 
 # cmod3.createReaction("R_VN", "Import S external to the cytosol", reversible=True)
 # cmod3.createReactionReagent("R_VN", cmod3.species[3].getId(), -1)
